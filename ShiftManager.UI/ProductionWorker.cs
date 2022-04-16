70fcdd3b29f72ba44501e29065733ce129ecafff
Kyle Zimmerman <kaz0010@auburn.com>
2022-04-16 12:53:34 -0700

    productionWorker class created

---
+++ ShiftManager.UI/ProductionWorker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftManager.UI
+{
+    class ProductionWorker : Employee
+    {
+        public int shiftNumber { get; set; }
+        public double hourlyRate { get; set; }
+    }
+}
