07061a43f43936644ec985fc25d21a01aa9cb2fc
Kyle Zimmerman <kaz0010@auburn.com>
2022-04-16 14:39:34 -0700

    project complete

--- ShiftManager.UI/Main.Designer.cs
+++ ShiftManager.UI/Main.Designer.cs
@@ -28,20 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelEmployeeNumber = new System.Windows.Forms.Label();
+            this.labelShiftNumber = new System.Windows.Forms.Label();
+            this.labelHourlyRate = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxEmployeeNumber = new System.Windows.Forms.TextBox();
+            this.textBoxShiftNumber = new System.Windows.Forms.TextBox();
+            this.textBoxHourlyRate = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.listBoxProductionWoker = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(100, 48);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // labelEmployeeNumber
+            // 
+            this.labelEmployeeNumber.AutoSize = true;
+            this.labelEmployeeNumber.Location = new System.Drawing.Point(100, 80);
+            this.labelEmployeeNumber.Name = "labelEmployeeNumber";
+            this.labelEmployeeNumber.Size = new System.Drawing.Size(93, 13);
+            this.labelEmployeeNumber.TabIndex = 1;
+            this.labelEmployeeNumber.Text = "Employee Number";
+            // 
+            // labelShiftNumber
+            // 
+            this.labelShiftNumber.AutoSize = true;
+            this.labelShiftNumber.Location = new System.Drawing.Point(100, 111);
+            this.labelShiftNumber.Name = "labelShiftNumber";
+            this.labelShiftNumber.Size = new System.Drawing.Size(68, 13);
+            this.labelShiftNumber.TabIndex = 2;
+            this.labelShiftNumber.Text = "Shift Number";
+            // 
+            // labelHourlyRate
+            // 
+            this.labelHourlyRate.AutoSize = true;
+            this.labelHourlyRate.Location = new System.Drawing.Point(100, 141);
+            this.labelHourlyRate.Name = "labelHourlyRate";
+            this.labelHourlyRate.Size = new System.Drawing.Size(63, 13);
+            this.labelHourlyRate.TabIndex = 3;
+            this.labelHourlyRate.Text = "Hourly Rate";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(199, 47);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(161, 20);
+            this.textBoxName.TabIndex = 4;
+            // 
+            // textBoxEmployeeNumber
+            // 
+            this.textBoxEmployeeNumber.Location = new System.Drawing.Point(199, 77);
+            this.textBoxEmployeeNumber.Name = "textBoxEmployeeNumber";
+            this.textBoxEmployeeNumber.Size = new System.Drawing.Size(161, 20);
+            this.textBoxEmployeeNumber.TabIndex = 5;
+            // 
+            // textBoxShiftNumber
+            // 
+            this.textBoxShiftNumber.Location = new System.Drawing.Point(199, 108);
+            this.textBoxShiftNumber.Name = "textBoxShiftNumber";
+            this.textBoxShiftNumber.Size = new System.Drawing.Size(161, 20);
+            this.textBoxShiftNumber.TabIndex = 6;
+            // 
+            // textBoxHourlyRate
+            // 
+            this.textBoxHourlyRate.Location = new System.Drawing.Point(199, 138);
+            this.textBoxHourlyRate.Name = "textBoxHourlyRate";
+            this.textBoxHourlyRate.Size = new System.Drawing.Size(161, 20);
+            this.textBoxHourlyRate.TabIndex = 7;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(452, 58);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(102, 39);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(452, 108);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(102, 39);
+            this.buttonSubmit.TabIndex = 9;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // listBoxProductionWoker
+            // 
+            this.listBoxProductionWoker.FormattingEnabled = true;
+            this.listBoxProductionWoker.Location = new System.Drawing.Point(120, 192);
+            this.listBoxProductionWoker.Name = "listBoxProductionWoker";
+            this.listBoxProductionWoker.Size = new System.Drawing.Size(451, 225);
+            this.listBoxProductionWoker.TabIndex = 10;
+            // 
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.listBoxProductionWoker);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textBoxHourlyRate);
+            this.Controls.Add(this.textBoxShiftNumber);
+            this.Controls.Add(this.textBoxEmployeeNumber);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelHourlyRate);
+            this.Controls.Add(this.labelShiftNumber);
+            this.Controls.Add(this.labelEmployeeNumber);
+            this.Controls.Add(this.labelName);
+            this.Name = "Main";
             this.Text = "Shift Manager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelEmployeeNumber;
+        private System.Windows.Forms.Label labelShiftNumber;
+        private System.Windows.Forms.Label labelHourlyRate;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxEmployeeNumber;
+        private System.Windows.Forms.TextBox textBoxShiftNumber;
+        private System.Windows.Forms.TextBox textBoxHourlyRate;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.ListBox listBoxProductionWoker;
     }
 }
 
--- ShiftManager.UI/Main.cs
+++ ShiftManager.UI/Main.cs
@@ -16,5 +16,80 @@ namespace ShiftManager.UI
         {
             InitializeComponent();
         }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // clear all textbox text
+            clearTextBoxes();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // create new ProductionWorker object
+                ProductionWorker worker = new ProductionWorker();
+
+                // check name text
+                checkName();
+
+                // set name
+                worker.employeeName = textBoxName.Text;
+
+                // set number
+                worker.employeeNumber = int.Parse(textBoxEmployeeNumber.Text);
+
+                // set shift
+                worker.shiftNumber = int.Parse(textBoxShiftNumber.Text);
+
+                // check for valid shift
+                checkShiftNumber(worker);
+
+                // set hourly rate
+                worker.hourlyRate = double.Parse(textBoxHourlyRate.Text);
+
+                // format string
+                string workerString = string.Format("Name: " + worker.employeeName + " | ID: " + worker.employeeNumber + " | Shift Number: " + worker.shiftNumber + " | Hourly Rate: " + worker.hourlyRate);
+
+                // add to list box
+                listBoxProductionWoker.Items.Add(workerString);
+
+                // clear textboxes 
+                clearTextBoxes();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Invalid input please review and submit again.");
+            }
+           
+        }
+
+        private void checkName()
+        {
+            // if name textbox is null or blank through exception
+            if (textBoxName.Text == null || textBoxName.Text == "")
+            {
+                throw new Exception();
+            }
+        }
+
+        private void checkShiftNumber(ProductionWorker worker)
+        {
+            // if shift number is not 1 or 2 through exception
+            if (worker.shiftNumber < 1 || worker.shiftNumber > 2)
+            {
+                throw new Exception();
+            }
+        }
+
+        private void clearTextBoxes()
+        {
+            // clear all textboxes
+            textBoxName.Text = "";
+            textBoxEmployeeNumber.Text = "";
+            textBoxShiftNumber.Text = "";
+            textBoxHourlyRate.Text = "";
+        }
+
     }
 }
