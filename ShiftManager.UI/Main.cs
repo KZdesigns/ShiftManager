4b37a47e62083589402b27ff719d67aa838ee960
Kyle Zimmerman <kaz0010@auburn.com>
2022-04-16 12:49:44 -0700

    employee class created

--- ShiftManager.UI/Main.cs
+++ ShiftManager.UI/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ShiftManager.UI
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
